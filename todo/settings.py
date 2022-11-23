7496a91e5a093040ff67d0bca583b85d7b4bc53f
Artashes <artshesblbulyan@gmail.com>
2022-11-23 21:47:48 +0400

    3

--- todo/settings.py
+++ todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&%%!*u&-ml46!68%x213q8g^2n9=2^g35ja*7j(6s5y$+f^c_q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #'192.168.2.172'
 ALLOWED_HOSTS = ['localhost','todoappmya.herokuapp.com']
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
